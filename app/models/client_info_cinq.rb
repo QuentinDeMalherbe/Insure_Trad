507c53a4a2407af4ea75ab117f7c0aedfbf476d9
Quentin De Malherbe <quentdemalherbe@gmail.com>
2021-03-10 10:33:56 +0100

    trad

--- app/models/client_info_cinq.rb
+++ app/models/client_info_cinq.rb
@@ -2,39 +2,39 @@ class ClientInfoCinq < ApplicationRecord
   belongs_to :police_cinq_million
 
   PAIEMENT = [
-    "Jusqu'à 30 jours date de facture",
-    "Jusqu'à 60 jours date de facture",
-    "Jusqu'à 75 jours date de facture",
-    "Jusqu'à 90 jours date de facture",
-    "Jusqu'à 120 jours date de facture",
-    "Plus de 120 jours date de facture"
+    ["Jusqu'à 30 jours date de facture", "Up to 30 days from invoice date"],
+    ["Jusqu'à 60 jours date de facture", "Up to 60 days from invoice date"],
+    ["Jusqu'à 75 jours date de facture", "Up to 75 days from invoice date"],
+    ["Jusqu'à 90 jours date de facture", "Up to 90 days from invoice date"],
+    ["Jusqu'à 120 jours date de facture", "Up to 120 days from invoice date"],
+    ["Plus de 120 jours date de facture", "More than 30 days from invoice date"]
   ]
 
   ANCIENNETE = [
-    "Nouveau  Client",
-    "< 1 an",
-    "entre 1 et  2  ans",
-    "entre 2 et  5  ans",
-    "plus de 5 ans"
+    ["Nouveau  Client", "New customer"],
+    ["< 1 an", "< 1 an"],
+    ["entre 1 et  2  ans", "between 1 and 2 years"],
+    ["entre 2 et  5  ans", "between 2 and 5 years"],
+    ["plus de 5 ans", "more than 5 years"]
   ]
 
   REGULARITE = [
-    "Au moins 1 livraison par semaine",
-    "Au moins 1 livraison par mois",
-    "Au moins 1 livraison par trimestre",
-    "Au moins 1 livraison tous les 6 mois",
-    "Irrégulier"
+    ["Au moins 1 livraison par semaine", "At least 1 delivery per week"],
+    ["Au moins 1 livraison par mois", "At least 1 delivery per month"],
+    ["Au moins 1 livraison par trimestre", "At least 1 delivery per trimester"],
+    ["Au moins 1 livraison tous les 6 mois", "At least 1 delivery every 6 months"],
+    ["Irrégulier", "Irregular"]
   ]
 
   EXPERIENCE = [
-    "Ponctuel",
-    "Parfois des retards",
-    "Souvent en retard ",
-    "Mauvais payeur"
+    ["Ponctuel", "Punctual"],
+    ["Parfois des retards", "Sometimes delays"],
+    ["Souvent en retard ", "Often late"],
+    ["Mauvais payeur", "Bad payer"]
   ]
 
   OUINON = [
-    "Oui",
-    "Non"
+    ["Oui", "Yes"],
+    ["Non", "No"]
   ]
 end
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -5,5 +5,7 @@ class Contract < ApplicationRecord
   has_one :insure_trad_supps
   has_one :police_xol
   validates :insure_trade_type , presence: true
-  INSURETRADTYPE = [['0', 'Police pour entreprise de moins de 5 millions € de chiffre d''affaires annuel'], ['1', 'Police XOL pour entreprise de 5 millions € à 20 millions € de chiffre d''affaires annuel'], ['2', 'Police d\'assurance second rang']]
+  INSURETRADTYPE = [['0', "Assurance des Impayés pour les artisans,commerçants, TPE/PMI avec un chiffre d'affaires jusquà 5 millions €", 'Bad Debts insurance for Smes up to € 5 million turnover'],
+  ['1', "Assurance des Impayés pour les PME avec un chiffre d'affaires entre  5 et 25 millions €", "Bad Debts insurance for medium size firms between  € 5 and 25 million turnover"],
+  ['2', 'Assurance des impayés en couverture de second rang', 'Bad debts Insurance secondary coverage']]
 end
