3d1e4c71bf3199fde36b121dd084bee8a9446dc5
Quentin De Malherbe <quentdemalherbe@gmail.com>
2022-08-23 18:22:30 +0200

    premier jet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_sign_up_params, if: :devise_controller?
   before_action :set_locale
 
   private
@@ -8,12 +9,34 @@ class ApplicationController < ActionController::Base
     I18n.locale = extract_locale || I18n.default_locale
   end
 
+
   def extract_locale
     parsed_locale = params[:locale]
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
 
   def default_url_options
-  { locale: I18n.locale }
-end
+    { locale: I18n.locale }
+  end
+
+  # devise
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.status == "expert"
+      profiles_intermediaire_home_path
+    elsif current_user.status == "assure"
+      profiles_insured_home_path
+    else
+      root_path
+    end
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:status])
+  end
+
+
 end
---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApplicationController
+
+  def insured_home
+
+  end
+
+  def intermediaire_home
+
+  end
+end
