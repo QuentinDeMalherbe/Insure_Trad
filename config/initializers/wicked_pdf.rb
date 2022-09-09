9cdd5268a4658027c32b6397f0777ef4d0d89cd1
Quentin De Malherbe <quentdemalherbe@gmail.com>
2022-09-09 16:21:59 +0200

    test

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -7,25 +7,22 @@
 # To learn more, check out the README:
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
+if Rails.env.development?
+  WickedPdf.config = {
+    # Path to the wkhtmltopdf executable: This usually isn't needed if using
+    # one of the wkhtmltopdf-binary family of gems.
+    # exe_path: '/usr/local/bin/wkhtmltopdf',
+    #   or
+    # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
-WickedPdf.config = {
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  # exe_path: '/usr/local/bin/wkhtmltopdf',
-  #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+    # Layout file to be used for all PDFs
+    # (but can be overridden in `render :pdf` calls)
+    # layout: 'pdf.html',
 
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html',
-
-  # Using wkhtmltopdf without an X server can be achieved by enabling the
-  # 'use_xvfb' flag. This will wrap all wkhtmltopdf commands around the
-  # 'xvfb-run' command, in order to simulate an X server.
-  #
-  # use_xvfb: true,
-}
-# config/initializers/wicked_pdf.rb
-WickedPdf.config.merge!({
-  layout: "pdf.html.erb",
-})
+    # Using wkhtmltopdf without an X server can be achieved by enabling the
+    # 'use_xvfb' flag. This will wrap all wkhtmltopdf commands around the
+    # 'xvfb-run' command, in order to simulate an X server.
+    #
+    # use_xvfb: true,
+  }
+end
